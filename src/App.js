072220ff0f76d8d21419cbd733018dfb14eadb67
Kabita Amgain <itsmekabu@gmail.com>
2023-05-14 17:39:18 +0545

    react router added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
 import logo from './logo.svg';
 import './App.css';
+import{Routes,Route,Link}from "react-router-dom";
+// import styles from"./App.module.css";
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Home from './components/pages/Home';
 
-function App() {
+// import'./style.css';
+
+
+/*export const App=()=>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+<h1>Hey</h1>
+</>
+  );
+}
+export const Application=()=>{
+  return (
+    <>
+<h1>What's up</h1>
+</>
   );
 }
 
+export const SecondApp=()=>{
+  return (
+    <>
+<h1>How are you?</h1>
+</>
+  );
+}*/
+
+
+// export const HomePage = () => {
+//   return <h1>This is a Home Page</h1>;
+// };
+// export const AboutPage = () => {
+//   return <h1>This is a About Page</h1>;
+// };
+// export const ContactPage = () => {
+//   return <h1>This is a Contact Page</h1>;
+// };
+ const App=()=>{
+return(
+    <>
+      <nav className="navbar">
+        <div className='navbrand'>Brand</div>
+        <ul className="navlist">
+        <li className='navlink'>
+          <Link 
+          to='/'
+          >
+          Home
+          </Link>
+          </li>
+        <li className='navlink'>
+        <Link 
+          to='/about'
+          >
+          AboutUs
+          </Link>
+          </li>
+        <li className='navlink'>
+        <Link 
+          to='/contact'
+          >
+          Contact
+          </Link>
+          </li>
+       </ul>
+      </nav>
+
+      
+      <Routes>
+       <Route index element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes>
+    </>
+)
+};
 export default App;
